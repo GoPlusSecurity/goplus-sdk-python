a385803469165471e69791353286ff3756a643e0
GoPlusBot <service@gopluslabs.io>
2023-05-29 10:08:03 +0000

    Update client code for 7757300334eae082556ecf7afad32254baae221e

--- swagger_client/api/__init__.py
+++ swagger_client/api/__init__.py
@@ -7,6 +7,7 @@ from swagger_client.api.approve_controller_v_1_api import ApproveControllerV1Api
 from swagger_client.api.approve_controller_v_2_api import ApproveControllerV2Api
 from swagger_client.api.contract_abi_controller_api import ContractAbiControllerApi
 from swagger_client.api.dapp_controller_api import DappControllerApi
+from swagger_client.api.defi_controller_api import DefiControllerApi
 from swagger_client.api.nft_controller_api import NftControllerApi
 from swagger_client.api.token_controller_api import TokenControllerApi
 from swagger_client.api.token_controller_v_1_api import TokenControllerV1Api
