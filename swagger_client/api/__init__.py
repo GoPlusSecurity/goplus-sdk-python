a2057e6f8b32c1af1862aa5f031bc26227854108
GoPlusBot <service@gopluslabs.io>
2023-06-15 18:01:48 +0000

    Update client code for 90bef199b9268b2b7e4009e70c975aece990bd26

--- swagger_client/api/__init__.py
+++ swagger_client/api/__init__.py
@@ -3,12 +3,5 @@ from __future__ import absolute_import
 # flake8: noqa
 
 # import apis into api package
-from swagger_client.api.approve_controller_v_1_api import ApproveControllerV1Api
-from swagger_client.api.approve_controller_v_2_api import ApproveControllerV2Api
-from swagger_client.api.contract_abi_controller_api import ContractAbiControllerApi
-from swagger_client.api.dapp_controller_api import DappControllerApi
-from swagger_client.api.defi_controller_api import DefiControllerApi
-from swagger_client.api.nft_controller_api import NftControllerApi
-from swagger_client.api.token_controller_api import TokenControllerApi
-from swagger_client.api.token_controller_v_1_api import TokenControllerV1Api
-from swagger_client.api.website_controller_api import WebsiteControllerApi
+from swagger_client.api.goplus_api_api import GoplusApiApi
+from swagger_client.api.goplus_token_api_api import GoplusTokenApiApi
