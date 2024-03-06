051e64fa0876d0a0b274c24ef8b7081d87673804
cong <1461561010@qq.com>
2024-03-06 17:45:22 +0800

    v0.2.3

--- goplus/__version__.py
+++ goplus/__version__.py
@@ -11,6 +11,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-VERSION = (0, 2, 2)
+VERSION = (0, 2, 3)
 
 __version__ = ".".join(map(str, VERSION))
