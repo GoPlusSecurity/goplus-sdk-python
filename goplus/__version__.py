ea1a80e33f1804c59632d18cee12102ffa367671
cong <xiluocong@gmail.com>
2026-08-21 09:53:24 +0800

    chore: release v0.2.6

--- goplus/__version__.py
+++ goplus/__version__.py
@@ -11,6 +11,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-VERSION = (0, 2, 4)
+VERSION = (0, 2, 6)
 
 __version__ = ".".join(map(str, VERSION))
